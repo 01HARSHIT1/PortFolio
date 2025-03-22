f48fcad16f0be9003607ebb483635cd010fbcb67
Harshit Rk <harsshitrk0120@gmail.com>
2025-03-22 20:08:40 +0530

    first commit

---
+++ Data/data.ts
@@ -0,0 +1,246 @@
+export const BaseInfo = {
+    name: "Harshit",
+    position: "Full Stack Web Developer",
+    description:
+      "I'm a passionate Full Stack Developer with expertise in building dynamic and scalable web applications using modern technologies. I love solving problems, creating seamless user experiences, and continuously learning new things. A positive and cheerful person, I enjoy collaborating with teams and bringing energy to every project I work on! 🚀😊",
+    profilePic: "/images/pp1.jpg",
+  };
+  
+  export const aboutInfo = {
+    title: "Crafting Web Application with Passion and Precision",
+    description:
+      "With a deep passion for crafting seamless and high-performance web applications, I specialize in building scalable, user-centric solutions with precision and efficiency. From front-end elegance to back-end robustness, I bring creativity, clean code, and modern technologies together to deliver exceptional digital experiences. 🚀",
+    client: "5+",
+    experience: "2+",
+    project: "25+",
+    website: "10+",
+  };
+  
+  export const servicesData = [
+    {
+      id: 1,
+      title: " Web Applications",
+      description: "Tailored web apps built with the latest technologies.",
+      icon: "/images/s1.png",
+    },
+    {
+      id: 2,
+      title: "E-commerce Websites",
+      description:
+        "Secure online stores with user-friendly shopping and payments.",
+      icon: "/images/s2.png",
+    },
+    {
+      id: 3,
+      title: "API Integration",
+      description:
+        "Seamless integration for smooth data sharing and functionality.",
+      icon: "/images/s3.png",
+    },
+    {
+      id: 4,
+      title: "User Interfaces",
+      description:
+        "Engaging, responsive front-end designs for better user experiences.",
+      icon: "/images/s4.png",
+    },
+    {
+      id: 5,
+      title: "Database Solutions",
+      description:
+        "Efficient and scalable database management for your data needs.",
+      icon: "/images/s5.png",
+    },
+    {
+      id: 6,
+      title: "Real-Time Features",
+      description: "Add live chat, notifications, and real-time data updates.",
+      icon: "/images/s6.png",
+    },
+    {
+      id: 7,
+      title: "Speed Optimization",
+      description: "Boost performance with faster load times and optimization.",
+      icon: "/images/s7.png",
+    },
+    {
+      id: 8,
+      title: "Website Management",
+      description: "Easy content management systems with flexible controls.",
+      icon: "/images/s8.png",
+    },
+  ];
+  
+  export const projectData = [
+    {
+      id: 1,
+      image: "/images/Sc1.png",
+      url: "https://tic-tak-toe-chi.vercel.app/",
+    },
+    {
+      id: 2,
+      image: "/images/Sc2.png",
+      url: "https://management-app-lime.vercel.app/",
+    },
+    {
+      id: 3,
+      image: "/images/Sc3.png",
+      url: "https://quiz-app-seven-pi.vercel.app/",
+    },
+    {
+      id: 4,
+      image: "/images/Sc4.png",
+      url: "https://investment-calculator-six-pi.vercel.app/",
+    },
+    {
+      id: 5,
+      image: "/images/Sc5.png",
+      url: "https://event-scheduler-app-olive.vercel.app/",
+    },
+    {
+      id: 6,
+      image: "/images/Sc6.png",
+      url: "https://food-order-app-nine-sigma.vercel.app/",
+    },
+    // {
+    //   id: 7,
+    //   image: "/images/p7.png",
+    //   url: "https://wdw-ecom.vercel.app/",
+    // },
+    // {
+    //   id: 8,
+    //   image: "/images/p8.png",
+    //   url: "https://webhost-ten.vercel.app/",
+    // },
+  ];
+  
+  export const skillsData = [
+    {
+      id: 1,
+      title: "React",
+      image: "/images/react.svg",
+      percent: "90%",
+    },
+    {
+      id: 2,
+      title: "CSS",
+      image: "/images/css.svg",
+      percent: "97%",
+    },
+    {
+      id: 3,
+      title: "JavaScript",
+      image: "/images/js.svg",
+      percent: "77%",
+    },
+    {
+      id: 4,
+      title: "TypeScript",
+      image: "/images/ts.svg",
+      percent: "67%",
+    },
+    {
+      id: 5,
+      title: "HTML",
+      image: "/images/html.svg",
+      percent: "67%",
+    },
+    {
+      id: 6,
+      title: "Node JS",
+      image: "/images/node.svg",
+      percent: "77%",
+    },
+    {
+      id: 7,
+      title: "MongoDB",
+      image: "/images/mongo.svg",
+      percent: "65%",
+    },
+    {
+      id: 8,
+      title: "Python",
+      image: "/images/python.svg",
+      percent: "55%",
+    },
+    {
+      id: 9,
+      title: "Firebase",
+      image: "/images/firebase.svg",
+      percent: "75%",
+    },
+    {
+      id: 10,
+      title: "Vue",
+      image: "/images/vue.svg",
+      percent: "35%",
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      name: "John Doe",
+      review:
+        "Exceptional service! The team delivered exactly what we needed on time. Very professional",
+      rating: 5,
+      profession: "Marketing Manager",
+      image: "/images/u1.jpg",
+    },
+    {
+      name: "Jane Smith",
+      review:
+        "Highly skilled developers who understand the client's vision and execute perfectly.",
+      rating: 4.8,
+      profession: "Business Owner",
+      image: "/images/u2.jpg",
+    },
+    {
+      name: "Michael Johnson",
+      review:
+        "Great communication and excellent results. Our new site performs beautifully.",
+      rating: 4.6,
+      profession: "Project Coordinator",
+      image: "/images/u3.jpg",
+    },
+    {
+      name: "Emily Brown",
+      review:
+        "Professional, efficient, and highly knowledgeable. Would definitely recommend their services!",
+      rating: 4.9,
+      profession: "Creative Director",
+      image: "/images/u4.jpg",
+    },
+  ];
+  
+  export const blogs = [
+    {
+      id: 1,
+      title: "Top Web Development Trends to Watch in 2024",
+      summary:
+        "Explore the key web development trends for 2024 and their impact on your business.",
+      date: "August 18, 2024",
+      image: "/images/b1.jpg",
+    },
+    {
+      id: 2,
+      title: "Why Your Business Needs a Custom Web Application",
+      summary:
+        "Discover why custom web applications are crucial for driving business growth and efficiency.",
+      date: "August 10, 2024",
+      image: "/images/b2.jpg",
+    },
+    {
+      id: 3,
+      title: "The Importance of Website Speed Optimization",
+      summary:
+        "Understand how website speed impacts user experience and learn essential optimization tips.",
+      date: "July 25, 2024",
+      image: "/images/b3.jpg",
+    },
+  ];
+  
+  export const contactData = {
+    phone: "+91(7717743862)",
+    email: "Harsshitrk0120@gmail.com",
+    address: "patna, Bihar, India",
+  };
---
+++ components/Home/Home.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useEffect } from 'react'
+import Hero from './Hero/Hero';
+import About from './About/About';
+import Services from './Services/Services';
+import Project from './Project/Project';
+import Skills from './Skills/Skills';
+import Slider from './Reviews/Slider';
+import Reviews from './Reviews/Reviews';
+import Blog from './BLOG/Blog';
+import Contact from './Contact/Contact';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const Home = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos");
+      AOS.init({
+        duration: 1000,
+        easing: "ease",
+        once: true,
+        anchorPlacement: "top-bottom",
+      });
+    };
+
+    initAOS();
+  }, []);
+
+  return (
+    <div className='overflow-hidden'>
+      <Hero />
+      <About />
+      <Services />
+      <Project />
+      <Skills />
+      <Reviews />
+      <Blog />
+      <Contact />
+    </div>
+  )
+}
+
+export default Home;
